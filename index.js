729d0a905622231f90efe523f3c3095145d9d8e9
Hassan <hassan.ali@g.austincc.edu>
2019-06-06 10:47:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+    x.style.position = "fixed";
+    x.style.zIndex = "999";
+  }
+}
